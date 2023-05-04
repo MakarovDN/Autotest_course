3c0d970a015b71502167725d86ce99b178d73132
dn.makarov <dn.makarov@tensor.ru>
2023-05-04 12:47:44 +0300

    new

---
+++ homework/Task_1.py
@@ -0,0 +1,9 @@
+# Ссылка на задание № 1. https://n.sbis.ru/shared/disk/b1cfedd8-32ec-41c0-add0-e1c45dde3adf
+
+name = 'Даниил'
+surname = 'Макаров'
+seniority_year = '3'
+seniority_month = '9'
+position = 'Инженер-тестировщик'
+print('Меня зовут', name, surname, '.\n'
+      'Я работаю в компании Тензор', seniority_year, 'года и', seniority_month, 'месяцев', 'на должности', position)
